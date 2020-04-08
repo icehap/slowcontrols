961cca32b601d98486b3952ad9f9fe7711406294
Colton21 <colton.hill@icecube.wisc.edu>
2020-04-08 17:45:03 +0900

    error logging script, paths hardcoded

---
+++ error_logging.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+import numpy as np
+import os, sys
+from termcolor import colored
+
+def update_log(logfile, errorfile, tags):
+
+    now = datetime.now()
+    f = open(logfile, "w")
+    t_tags = str(tags['Tags'])
+    t_name = str(tags['Name'])
+    t_num = str(tags['Num'])
+    write_string = str(now) + ", " + t_tags + ", " + t_name + ", " + t_num
+    try:
+        f.write(write_string)
+    except:
+        raise IOError
+    f.close()
+    print("Log file updated")
+
+def get_info(num):
+
+    info = []
+    tags = {}
+    print("Please Input the Error Details...")
+    info = str(input("Please Describe the Error: "))
+
+    tag = input("Please give a brief list of tags (meta-info): ")
+    user = input("Please Give your Name: ")
+    tags['Name'] = user
+    tags['Tags'] = tag
+    tags['Num'] = num
+
+    return (info, tags)
+
+def new_error(logfile):
+    num = len(os.listdir("/home/icehap-daq/errors/"))
+    path = f"/home/icehap-daq/errors/err_{num}.txt"
+    if os.path.exists(path):
+        inpt = input("You're overwriting " + path + " - Proceed? [y/n]")
+        if inpt.lower() not in ["y", "yes"]:
+            print("Not overwriting file...")
+            exit(1)
+    new_error_file = path
+    f = open(new_error_file, "w+")
+    print(f"Creating new error file: {new_error_file}")
+    
+    info, tags = get_info(num)
+    f.write(info)
+    f.close()
+
+    update_log(logfile, new_error_file, tags)
+
+if __name__ == "__main__":
+
+    choice = input("Logging an error? [y/n]: ")
+    if choice.lower() not in ["y", "yes"]:
+        print("Go home and rethink your life")
+        exit(1)
+
+    print(colored("If this is a software error, please also provide the error!", 'blue'))
+
+    logfile = "/home/icehap-daq/errors/log.txt"
+
+    new_error(logfile) 
+
+##end
