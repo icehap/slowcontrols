192e104d84e3b6fc447ed435e57a41c7b3dfcd9c
Colton Hill <colton.hill@icecube.wisc.edu>
2022-01-11 15:44:59 +0900

    Update setup.py
    URL updated to reflect position in slowcontrols repository

--- slackbot/setup.py
+++ slackbot/setup.py
@@ -4,6 +4,6 @@ setup(name='chiba_slackbot',
       version='0.1',
       description='Push messages to Chiba Slackbot',
       author='Icehap',
-      url='https://github.com/icehap/slackbot',
+      url='https://github.com/icehap/slowcontrols/slackbot',
       install_requires=['slackclient'],
       packages=['chiba_slackbot'])
